9fcae42dbadfb8b638b90dd3487bf3bdcc6f5782
mariasiqueiraferreira-source <maria.siqueira.ferreira@escola.pr.gov.br>
2026-07-04 19:25:00 -0300

    s

--- script.js
+++ script.js
@@ -1,30 +1,11 @@
-// Guarda o nome do usuário (uso de variável como pede a rubrica)
-let nomeUsuario = "Visitante";
+const avanca = document.querySelectorAll('.btn-proximo');
 
-// Seleciona elementos do HTML
-const botaoModo = document.getElementById("btnModo");
-const body = document.body;
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-// Cria um elemento para mensagem dinâmica
-const mensagem = document.createElement("p");
-mensagem.textContent = "Olá, " + nomeUsuario + "! Bem-vindo ao site sobre agricultura regenerativa.";
-document.body.insertBefore(mensagem, document.body.firstChild);
-
-// Função para alternar modo escuro
-function alternarModo() {
-    body.classList.toggle("modo-escuro");
-
-    if (body.classList.contains("modo-escuro")) {
-        botaoModo.textContent = "Modo claro";
-    } else {
-        botaoModo.textContent = "Modo escuro";
-    }
-}
-
-// Evento no botão (interação com DOM)
-botaoModo.addEventListener("click", alternarModo);
-
-// Exemplo simples de interação adicional (usabilidade)
-mensagem.addEventListener("click", function () {
-    mensagem.textContent = "Agricultura regenerativa transforma o solo e o futuro 🌱";
-});
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
